cf330f74a366a0588f8a10f2e4b273cef66b196d
christycrt <christycrt@gmail.com>
2022-03-26 00:44:23 +0700

    [UPDATE] footer layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,27 +22,35 @@ const data = [
 
 const Footer = () => {
   return (
-    <div className="flex flex-col text-center bg-very-dark-blue py-16">
-      <div className="flex justify-center mb-16">
-        <img className="logo1 w-[121px]" src="images/logo.svg" alt="logo" />
-      </div>
-      {data.map((item) => {
-        return (
-          <div key={item.title}>
-            <p className="font-bold text-white mb-6">{item.title}</p>
-            <div className="flex flex-col text-gray space-y-2 mb-10">
-              {item.links.map((link) => {
-                return <a key={link}>{link}</a>;
-              })}
-            </div>
+    <div className="bg-very-dark-blue">
+      <div className="flex flex-col text-center py-16 lg:container lg:grid lg:grid-cols-[4fr_2fr_2fr_2fr_2fr] lg:text-left">
+        <div className="flex justify-center mb-16 lg:justify-start lg:mb-0">
+          <div>
+            <img
+              className="logo-footer w-[121px] lg:h-auto"
+              src="images/logo.svg"
+              alt="logo"
+            />
           </div>
-        );
-      })}
-      <div className="flex justify-center text-white space-x-6">
-        <FaFacebookSquare className="text-2xl" />
-        <FaTwitter className="text-2xl" />
-        <FaPinterest className="text-2xl" />
-        <FaInstagram className="text-2xl" />
+        </div>
+        {data.map((item) => {
+          return (
+            <div key={item.title}>
+              <p className="font-bold text-white mb-6">{item.title}</p>
+              <div className="flex flex-col text-gray space-y-2 mb-10">
+                {item.links.map((link) => {
+                  return <a className="hover:text-cyan cursor-pointer" key={link}>{link}</a>;
+                })}
+              </div>
+            </div>
+          );
+        })}
+        <div className="flex justify-center text-white space-x-6 lg:justify-end">
+          <FaFacebookSquare className="text-2xl hover:text-cyan cursor-pointer" />
+          <FaTwitter className="text-2xl hover:text-cyan cursor-pointer" />
+          <FaPinterest className="text-2xl hover:text-cyan cursor-pointer" />
+          <FaInstagram className="text-2xl hover:text-cyan cursor-pointer" />
+        </div>
       </div>
     </div>
   );

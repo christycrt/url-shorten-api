7329e2a7306dba068ff0300d08cbf5cba6642668
christycrt <christycrt@gmail.com>
2022-03-26 00:50:45 +0700

    [UPDATE] features layout desktop

--- components/Features/FeatureCard.tsx
+++ components/Features/FeatureCard.tsx
@@ -7,11 +7,11 @@ interface Props {
 }
 const FeatureCard: React.FC<Props> = ({ data }) => {
   return (
-    <div className="text-center relative">
-      <div className="bg-dark-violet p-5 rounded-full inline-block absolute left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="text-center relative lg:text-left">
+      <div className="bg-dark-violet p-5 rounded-full inline-block absolute left-1/2 -translate-x-1/2 -translate-y-1/2 lg:left-8 lg:translate-x-0">
         <img src={data.icon} alt="icon-brand" />
       </div>
-      <div className="bg-white px-8 pb-8 pt-16 rounded-md">
+      <div className="bg-white px-8 pb-8 pt-16 rounded-md lg:pt-20">
         <p className="font-bold text-xl mb-4">{data.title}</p>
         <p className="text-grayish-violet">{data.detail}</p>
       </div>
--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -8,28 +8,43 @@ const Features = () => {
     <div className="bg-grayish">
       <div className="container pb-20">
         <ShortenLinkList />
-        <p className="font-bold text-2xl text-center mb-6 text-very-dark-blue">
+        <p className="font-bold text-2xl text-center mb-6 text-very-dark-blue lg:text-3xl">
           Advanced Statistics
         </p>
-        <p className="text-center text-grayish-violet mb-20">
+        <p className="text-center text-grayish-violet mb-20 lg:px-80">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
-        {featureData.map((item, key) => {
-          return (
-            <div key={key}>
-              <div
-                className={`${
-                  key === 0 ? "hidden" : "flex mx-auto h-20 w-2 bg-cyan"
-                }`}
-              ></div>
-              <FeatureCard data={item} />
-            </div>
-          );
-        })}
+        <div className="lg:grid lg:grid-cols-3 lg:gap-8 lg:relative">
+          <div className="bg-cyan h-2 absolute w-full top-[45%] hidden lg:block"></div>
+          {featureData.map((item, key) => {
+            let paddingTop;
+            switch (key) {
+              case 1:
+                paddingTop = "lg:pt-12";
+                break;
+              case 2:
+                paddingTop = "lg:pt-24";
+                break;
+              default:
+                paddingTop = "lg:pt-0";
+                break;
+            }
+            return (
+              <div className={`${paddingTop}`} key={key}>
+                <div
+                  className={`lg:hidden ${
+                    key === 0 ? "hidden" : "flex mx-auto h-20 w-2 bg-cyan"
+                  }`}
+                ></div>
+                <FeatureCard data={item} />
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="boost-box">
-        <p className="text-2xl font-bold text-white text-center mb-6">
+        <p className="text-2xl font-bold text-white text-center mb-6 lg:text-3xl">
           {" "}
           Boost your links today
         </p>
